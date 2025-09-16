62b96a0edb53a4a56928821d51a68278f72e5e65
ZbindenDaniel <zbindendaniel92@gmail.com>
2025-09-16 21:50:04 +0200

    cherry pick away strange things from main

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: [
-    '**/backend/**/*.test.ts',
-    '**/v2/backend/**/*.test.ts',
-    '**/test/**/*.test.ts'
-  ],
+  testMatch: ['**/backend/**/*.test.ts'],
 };
