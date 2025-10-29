bc7ba649f6ddf27e2ba3977b818b6ede6b8dd045
Daniel Zbinden <88660575+ZbindenDaniel@users.noreply.github.com>
2025-10-29 09:44:30 +0100

    Remove legacy Shopware integration module

--- backend/integrations/shopware/index.ts
+++
@@ -1,5 +0,0 @@
-export { ShopwareClient } from './client';
-export type { ShopwareClientOptions, ShopwareProductPayload, ShopwareRetryConfig } from './types';
-export type { ShopwareProductCustomFields, ShopwareItemProjection } from './types';
-export { mapItemProjectionToShopwareProduct, mapItemToShopwareProduct, mapShopwareProductToItemProjection, mapShopwareProductToItemRef } from './mappers';
-export { ShopwareError, ShopwareAuthError, ShopwareNetworkError, ShopwareRequestError } from './errors';
